6dd51ba2fb6c535e9210e1e1f7786a921492b013
rumeysaozturk <74792539+rumeysaozturk@users.noreply.github.com>
2021-09-16 17:09:08 +0300

    Update CarRepository.java

--- spring-boot/src/main/java/com/example/springboot/CarRepository.java
+++ spring-boot/src/main/java/com/example/springboot/CarRepository.java
@@ -1,8 +1,19 @@
 package com.example.springboot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.springboot.User;
 
 public interface CarRepository extends JpaRepository<Car,Integer> {
 
+	@Query(value="select count(*) from cars c where c.plaque=:plaque",nativeQuery=true)
+	int findByPlaque(String plaque);
+	@Query(value="select * from cars c where c.plaque=:plaque",nativeQuery=true)
+	Car findByPlaqueCar(String plaque);
+	@Query(value="select year from cars c where c.plaque=:plaque",nativeQuery=true)
+	Long findByYear(String plaque);
+	@Query(value="select brand from cars c where c.plaque=:plaque",nativeQuery=true)
+	String findByBrand(String plaque);
+	@Query(value="select model from cars c where c.plaque=:plaque",nativeQuery=true)
+	String findByModel(String plaque);
 }
