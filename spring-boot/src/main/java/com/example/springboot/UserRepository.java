cfb52c9a58465ddb72b58ab3ba213f9473d2dee6
rumeysaozturk <74792539+rumeysaozturk@users.noreply.github.com>
2021-09-16 17:10:43 +0300

    Update UserRepository.java

--- spring-boot/src/main/java/com/example/springboot/UserRepository.java
+++ spring-boot/src/main/java/com/example/springboot/UserRepository.java
@@ -1,9 +1,15 @@
 package com.example.springboot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.springboot.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+	@Query(value="select count(*) from users u where u.tc=:tc",nativeQuery=true)
+	int findByTc(String tc);
+	
+	@Query(value="select * from users u where u.tc=:tc",nativeQuery=true)
+	User findByTcUser(String tc);
 }
