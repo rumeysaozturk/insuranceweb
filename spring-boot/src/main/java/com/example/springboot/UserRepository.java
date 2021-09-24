833ed7825e10527d9f7201de420990193da66607
rumeysaozturk <74792539+rumeysaozturk@users.noreply.github.com>
2021-09-24 17:53:06 +0300

    Update UserRepository.java

--- spring-boot/src/main/java/com/example/springboot/UserRepository.java
+++ spring-boot/src/main/java/com/example/springboot/UserRepository.java
@@ -12,4 +12,10 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	@Query(value="select * from users u where u.tc=:tc",nativeQuery=true)
 	User findByTcUser(String tc);
+	
+
+	@Query(value="select * from users u where u.user_id=:id",nativeQuery=true)
+	User findByIdUser(int id);
+	//@Query(value="update users u set u.name:name")
+//	void updateUser(int id);
 }
